224e1360ff5fffe475489666b5670db6d1077d4a
Ramiro Alejandro Alvarez <ramiro.al.alvarez@gmail.com>
2024-04-16 01:04:08 -0300

    add: first endpoint (signup users)

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
-import express, { type Express, type Request, type Response } from "express"
+import express, { type Express, type Request, type Response } from "express";
+import prisma from "../prisma/client";
 
-const app: Express = express()
-const port = process.env.PORT || 3000
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Trying update')
-})
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+app.get("/", async (req: Request, res: Response) => {
+  await prisma.user.create({
+    data: 
+      {
+        name: "roberto",
+        email: "roberto09@gmail.com",
+        profile_image: "123123.jpg",
+        hashed_password: "123912",
+      },
+    });
+  res.send("Trying update");
+});
+
+app.get("/userinfo", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
+});
+
