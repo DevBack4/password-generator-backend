cee2df76bbe3bcf9bd10280f5debbeabe1da3c1d
Ramiro Alejandro Alvarez <ramiro.al.alvarez@gmail.com>
2024-04-17 23:31:36 -0300

    add-user endpoint agregate

---
+++ prisma/migrations/20240418020730_adding_default_hashed_password/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "hashed_password" SET DEFAULT 'hashed password';
--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,37 @@ import express, { type Express, type Request, type Response } from "express";
 import prisma from "../prisma/client";
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
 app.get("/", async (req: Request, res: Response) => {
-  await prisma.user.create({
-    data: 
-      {
-        name: "roberto",
-        email: "roberto09@gmail.com",
-        profile_image: "123123.jpg",
-        hashed_password: "123912",
-      },
-    });
-  res.send("Trying update");
+  res.send("");
 });
 
-app.get("/userinfo", async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.send(users);
+// ADD - USER
+app.post("/add-user", async (req: Request, res: Response) => {
+
+  const { userName, userEmail, userPassword } = req.body;
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: userName,
+        email: userEmail,
+        password: userPassword,
+      },
+    });
+
+    res.send(  {
+      "status":"success",
+      "user": userName,
+      "email": userEmail  
+      
+    });
+
+  } catch (error: ErrorEvent | any) { res.send("Something was wrong!")};
 });
 
